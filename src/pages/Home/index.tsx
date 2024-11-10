e31240b2bb88b0cc8d36eaf9e6dcdf4302d8e7ce
Andy Maskiell <maskiella@gmail.com>
2024-11-10 12:40:23 -0500

    Create Home page component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { ReactLogo, ViteLogo } from '@src/components';
+
+const Home = () => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <div>
+                <ViteLogo />
+                <ReactLogo />
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </div>
+    );
+};
+
+export default Home;
