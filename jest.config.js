7d71a832b91f91349ec7738be32a630323190389
Andy Maskiell <maskiella@gmail.com>
2024-12-01 16:50:56 -0500

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
+    coveragePathIgnorePatterns: ['/dist/', '/node_modules/', '**/*.d.ts'],
     moduleDirectories: ['node_modules'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
     moduleNameMapper: {
         "\\.(css|less|sass|scss)$": "identity-obj-proxy",
         "^.+\\.svg$": "jest-transformer-svg",
     },
-    // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    // setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
     // testEnvironment: "jsdom",
     transform: {
         "^.+.tsx?$": ["ts-jest", {}],
