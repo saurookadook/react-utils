2dea510ea6557ecdcc82cdcd0874e13c72f66d88
Andy Maskiell <maskiella@gmail.com>
2024-12-31 17:23:36 -0500

    Create initial files for render-with-context

---
+++ packages/render-with-context/__tests__/index.test.tsx
@@ -0,0 +1,7 @@
+'use strict';
+
+const reactUtilsRenderWithContext = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(reactUtilsRenderWithContext(), 'Hello from reactUtilsRenderWithContext');
+console.info('reactUtilsRenderWithContext tests passed');
